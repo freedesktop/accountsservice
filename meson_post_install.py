880976395dc45ef59e06b227cbba70d9861f2e71
Philip Withnall <withnall@endlessm.com>
2019-01-16 14:22:57 +0000

    build: Ensure state directories are installed with correct permissions
    See the previous commit.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- meson_post_install.py
+++ meson_post_install.py
@@ -9,10 +9,10 @@ localstatedir = os.path.normpath(destdir + os.sep + sys.argv[1])
 # FIXME: meson will not track the creation of these directories
 #        https://github.com/mesonbuild/meson/blob/master/mesonbuild/scripts/uninstall.py#L39
 dst_dirs = [
-  os.path.join(localstatedir, 'lib', 'AccountsService', 'icons'),
-  os.path.join(localstatedir, 'lib', 'AccountsService', 'users'),
+  (os.path.join(localstatedir, 'lib', 'AccountsService', 'icons'), 0o775),
+  (os.path.join(localstatedir, 'lib', 'AccountsService', 'users'), 0o700),
 ]
 
-for dst_dir in dst_dirs:
+for (dst_dir, dst_dir_mode) in dst_dirs:
   if not os.path.exists(dst_dir):
-    os.makedirs(dst_dir)
+    os.makedirs(dst_dir, mode=dst_dir_mode)
