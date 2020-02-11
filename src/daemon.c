freedesktop/accountsservice

commit 4b3fdd19588e429867c1305a9cdfd26e991f7f07

daemon: Write GDM custom.conf file if it doesn't exist
The previous code would abort the write if it didn't exist.

src/daemon.c
@@ -1522,13 +1522,18 @@ save_autologin (Daemon      *daemon,
         g_autoptr(GKeyFile) keyfile = NULL;
         g_autofree gchar *data = NULL;
         gboolean result;
+        g_autoptr(GError) local_error = NULL;
 
         keyfile = g_key_file_new ();
         if (!g_key_file_load_from_file (keyfile,
                                         PATH_GDM_CUSTOM,
                                         G_KEY_FILE_KEEP_COMMENTS,
                                         error)) {
-                return FALSE;
+                /* It's OK for custom.conf to not exist, we will make it */
+                if (!g_error_matches (local_error, G_FILE_ERROR, G_FILE_ERROR_NOENT)) {
+                        g_propagate_error (error, g_steal_pointer (&local_error));
+                        return FALSE;
+                }
         }
 
         g_key_file_set_string (keyfile, "daemon", "AutomaticLoginEnable", enabled ? "True" : "False");

commit bd3de96029a5af64b699af8e626d9742d8ba2d69

daemon: Fix error check in save_autologin()
This regressed in 4b3fdd19.

src/daemon.c
@@ -1542,7 +1542,7 @@ save_autologin (Daemon      *daemon,
         if (!g_key_file_load_from_file (keyfile,
                                         PATH_GDM_CUSTOM,
                                         G_KEY_FILE_KEEP_COMMENTS,
-                                        error)) {
+                                        &local_error)) {
                 /* It's OK for custom.conf to not exist, we will make it */
                 if (!g_error_matches (local_error, G_FILE_ERROR, G_FILE_ERROR_NOENT)) {
                         g_propagate_error (error, g_steal_pointer (&local_error));